6801223fa8a13b5c8d1312987c850b5ada642757
RioTheDev <dateshidze.gio@gmail.com>
2025-01-25 17:42:37 +0400

    final function

--- Assets/gioskriptebi/bushtebi_vizuali.cs
+++ Assets/gioskriptebi/bushtebi_vizuali.cs
@@ -11,7 +11,7 @@ public class bushtebi_vizuali : MonoBehaviour
     {
         GameObject spawned = Instantiate(Balloon, transform.position, transform.rotation);
         spawned.GetComponent<SpriteRenderer>().color = Random.ColorHSV();
-        Color lightColor = Random.ColorHSV(0, 1, 0.5f, 1, 0.7f, 1);
+        Color lightColor = Random.ColorHSV(0, 1, 0.5f, 1, 0.9f, 1);
         spawned.GetComponent<SpriteRenderer>().color = lightColor;
         spawned.transform.SetParent(transform);
         spawned.transform.localPosition = new Vector3(Random.Range(-0.25f, 0.25f), 0, 0);
--- Assets/gioskriptebi/bushti.cs
+++ Assets/gioskriptebi/bushti.cs
@@ -6,20 +6,35 @@ public class bushti_line : MonoBehaviour
 {
     LineRenderer lr;
     private GameObject player;
+    private Rigidbody2D playerrb;
     Vector3 direction;
+    Vector3 startpos;
 
     // Start is called before the first frame update
     void Start()
     {
+        startpos = transform.localPosition;
         lr = GetComponent<LineRenderer>();
         player = GameObject.FindWithTag("Player");
-
+        playerrb = player.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Make the balloon follow slowly
+        //Make the balloon follow 
+        if ((playerrb.velocity * Vector3.right).magnitude > 0.1f)
+        {
+            float velocity_direction = playerrb.velocity.normalized.x;
+            transform.localPosition = startpos * velocity_direction + 0.5f * velocity_direction * Vector3.left;
+        }
+        else
+        {
+            transform.localPosition = startpos;
+        }
+
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, (transform.position - player.transform.position).normalized);
+
         lr.SetPosition(0, transform.position);
         lr.SetPosition(1, player.transform.position);
         lr.startColor = Color.Lerp(Color.white, Color.gray, 0.5f);
